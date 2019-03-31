6cf165bdf3d98a70be9ff94215357012f47471f5
Moozgu <rukagilga@gmail.com>
2019-03-31 11:39:20 +0200

    FInnally some task

--- src/Tasks/Book/Data/BookRepository.java
+++ src/Tasks/Book/Data/BookRepository.java
@@ -9,20 +9,21 @@ public class BookRepository {
         private List<Book> bookRepository = new LinkedList<>();
         private static BookRepository repositoryInstance = null;
 
-//        bookRepository.add(new Book ("Pirates",600,120));
-//        bookRepository.add(new Book ("Anders",150,240));
-//        bookRepository.add(new Book ("Nevernding Story",680,90));
-//        bookRepository.add(new Book ("Santanders",130,68));
-//        bookRepository.add(new Book ("Zalazar",420,200));
-//        bookRepository.add(new Book ("ET",985,350));
-//        bookRepository.add(new Book ("Null",142,140));
-//        bookRepository.add(new Book ("Barbarians",324,16));
+
 
     private BookRepository() {
+        this.bookRepository.add(new Book ("Pirates",600,120));
+        this.bookRepository.add(new Book ("Anders",150,240));
+        this.bookRepository.add(new Book ("Nevernding Story",680,90));
+        this.bookRepository.add(new Book ("Santanders",130,68));
+        this.bookRepository.add(new Book ("Zalazar",420,200));
+        this.bookRepository.add(new Book ("ET",985,350));
+        this.bookRepository.add(new Book ("Null",142,140));
+        this.bookRepository.add(new Book ("Barbarians",324,16));
+        this.addBook(new Book ("Name of the WInd", 800,100));
     }
 
     public void addBook(Book book){
-
     this.bookRepository.add(book);
 }
     public static BookRepository getBookRepositoryInstance(){
