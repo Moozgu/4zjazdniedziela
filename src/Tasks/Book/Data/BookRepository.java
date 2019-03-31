e5b07e8b8d9ce16bbbcffde39ab82f5491662627
Moozgu <rukagilga@gmail.com>
2019-03-31 11:23:35 +0200

    FInnally some task

--- src/Tasks/Book/Data/BookRepository.java
+++ src/Tasks/Book/Data/BookRepository.java
@@ -2,25 +2,39 @@ package Tasks.Book.Data;
 
 import Tasks.Book.Model.Book;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class BookRepository {
-    public static void main(String[] args) {
-        List<Book> bookList = new ArrayList<>();
+        private List<Book> bookRepository = new LinkedList<>();
+        private static BookRepository repositoryInstance = null;
 
-        bookList.add(new Book ("Pirates",600,120));
-        bookList.add(new Book ("Anders",150,240));
-        bookList.add(new Book ("Nevernding Story",680,90));
-        bookList.add(new Book ("Santanders",130,68));
-        bookList.add(new Book ("Zalazar",420,200));
-        bookList.add(new Book ("ET",985,350));
-        bookList.add(new Book ("Null",142,140));
-        bookList.add(new Book ("Barbarians",324,16));
+//        bookRepository.add(new Book ("Pirates",600,120));
+//        bookRepository.add(new Book ("Anders",150,240));
+//        bookRepository.add(new Book ("Nevernding Story",680,90));
+//        bookRepository.add(new Book ("Santanders",130,68));
+//        bookRepository.add(new Book ("Zalazar",420,200));
+//        bookRepository.add(new Book ("ET",985,350));
+//        bookRepository.add(new Book ("Null",142,140));
+//        bookRepository.add(new Book ("Barbarians",324,16));
 
-        for (Book book : bookList) {
-            System.out.println(book);
+    private BookRepository() {
+    }
+
+    public void addBook(Book book){
+
+    this.bookRepository.add(book);
+}
+    public static BookRepository getBookRepositoryInstance(){
+        if(BookRepository.repositoryInstance == null){
+            BookRepository.repositoryInstance = new BookRepository();
 
         }
+        return BookRepository.repositoryInstance;
     }
+
+
+
+
 }
+
--- src/Tasks/Book/Main.java
+++ src/Tasks/Book/Main.java
@@ -1,4 +1,12 @@
 package Tasks.Book;
 
+import static Tasks.Book.Data.BookRepository.getBookRepositoryInstance;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+        getBookRepositoryInstance();
+
+    }
 }
